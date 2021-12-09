b52dbccbeb350997ff1a0d5ea73d3e4025999f5f
Bruno Menslin <brunomenslin@outlook.com>
2021-12-09 12:30:28 -0300

    feat: update app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { Header } from '../components/Header'
+import { AuthProvider } from '../contexts/AuthContext'
+import styles from '../styles/app.module.scss'
+
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <AuthProvider>
+            <Head>
+                <title>HATED</title>
+                <meta name="description" content="Skate Spot Share" />
+            </Head>
+            <div className={styles.wrapper}>
+                <main>
+                    <Header />
+                    <Component {...pageProps} />
+                </main>
+            </div>
+        </AuthProvider>
+    )
 }
 
 export default MyApp
