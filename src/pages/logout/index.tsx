da7fd7d7117e6187bdc97850f9e475c4016ad7f8
Bruno Menslin <brunomenslin@outlook.com>
2021-12-09 17:44:38 -0300

    fix: logout page destroy cookies

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -1,14 +1,11 @@
 import { GetServerSideProps } from "next"
-import nookies from 'nookies'
-import { useCookies } from "react-cookie"
+import nookies, { destroyCookie } from 'nookies'
 import Router from 'next/router'
+import { useEffect } from "react";
 
 
 export default function Logout() {
-
-    const [cookies, setCookie, removeCookie] = useCookies(['hated-token']);
-    removeCookie('hated-token') //client cookies
-
+    destroyCookie(null, 'hated-token', { path: '/' })
     Router.push('/')
 
     return (
